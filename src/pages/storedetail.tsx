d9eb7995f4aec4aba335df9cb615ebe1075b8c84
김동우 <system421@gmail.com>
2024-07-24 15:18:41 +0900

    feat:가게상세페이지 추가, fix:메뉴 상세페이지 수정 (#32)

---
+++ src/pages/storedetail.tsx
@@ -0,0 +1,9 @@
+import StoreDetail_idx from "./components/storedetail_idx"
+const StoreDetail = () => {
+    return(
+        <div>
+        <StoreDetail_idx> </StoreDetail_idx>
+        </div>
+    )
+    }
+    export default StoreDetail;
