fdd8d816cb7c82dcca95a36148c819d51b9da350
김동우 <system421@gmail.com>
2024-07-24 13:54:14 +0900

    feat:메뉴상세보기화면 추가 (#27)
     feat:메뉴 상세보기 화면의 초안을 작성하였습니다.

---
+++ src/pages/menudetail.tsx
@@ -0,0 +1,9 @@
+import MenuDetail_idx from "./components/menudetail_idx"
+const MenuDetail = () => {
+return(
+    <div>
+    <MenuDetail_idx></MenuDetail_idx>
+    </div>
+)
+}
+export default MenuDetail;
