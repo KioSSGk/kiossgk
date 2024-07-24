f38ea4c12b9c248ceae18dbd97d3a47be7bd6894
LeeDongUk <144418233+leedonguk0809@users.noreply.github.com>
2024-07-24 23:01:09 +0900

    feat : 어드민 메인 화면 개발 (#35)
    feat : 어드민 메인 화면 페이지 개발

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+// src/pages/_app.tsx
 import "@/styles/globals.css";
+import "react-calendar/dist/Calendar.css"; // React Calendar 기본 스타일
+import "@/styles/CalendarStyles.css"; // 캘린더 스타일 추가
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
