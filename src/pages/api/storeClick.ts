040258ef4f4755b4a6a58217e940453799198c92
10wook <younguk137@naver.com>
2024-07-29 14:49:11 +0900

    hyw/feat/mainToStoreBtn

--- src/pages/api/storeClick.ts
+++ src/pages/api/storeClick.ts
@@ -7,6 +7,16 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
         // 클릭된 가게 ID를 터미널에 출력
         console.log('Clicked store ID:', id);
 
+
+        // 리디렉션 응답
+        res.writeHead(302, {
+            Location: `/storedetail` // 리디렉션할 페이지 경로
+        });
+                
+        // 성공적으로 출력되었음을 응답
+        res.status(200).json({ message: 'Store click logged.' });
+                res.end();
+
         // 성공적으로 출력되었음을 응답
         res.status(200).json({ message: 'Store click logged.' });
     } else {
