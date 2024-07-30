5512f836a1c3ff829a041b04fd4c416f3eac9fad
한영욱 <70461240+10wook@users.noreply.github.com>
2024-07-30 15:12:07 +0900

    hyw/split/adminStore (#67)
    fix #56 어드민 회원가입과 가게 등록을 분리하여 구한하였습니다.

---
+++ src/pages/adminSignup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminSignupForm from './components/admin_signup/adminSignupForm';
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <AdminSignupForm/>
+        </div>
+    );
+};
+
+export default SignUpPage;
