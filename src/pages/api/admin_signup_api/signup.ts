34bb4000da0580d56ca72fb65f0119a2f67ab670
leedonguk0809 <ehddnr0481@naver.com>
2024-08-02 17:36:52 +0900

    Fix : 가게번호 조회 버그수정

--- src/pages/api/admin_signup_api/signup.ts
+++ src/pages/api/admin_signup_api/signup.ts
@@ -14,16 +14,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
             // DB에 삽입
             const [result] = await pool.query(
-                `INSERT INTO Admin (email, admin_name, passwd, phone,  use_state) VALUES (?, ?, ?, ?, ?)`,
+                `INSERT INTO Admin (email, admin_name, passwd, phone, use_state) VALUES (?, ?, ?, ?, ?)`,
                 [email, name, hashedPassword, phoneNumber, 1]
             );
 
-            res.status(200).json({ message: '회원가입이 완료되었습니다.' });
+            const adminIdx = (result as any).insertId;
+
+            // 쿠키 설정
+            res.setHeader('Set-Cookie', `adminIdx=${adminIdx}; Path=/; HttpOnly`);
+
+            return res.status(200).json({ message: '회원가입이 완료되었습니다.', adminIdx });
         } catch (error) {
             console.error('회원가입 중 오류 발생:', error);
-            res.status(500).json({ message: '서버 오류가 발생했습니다.' });
+            return res.status(500).json({ message: '서버 오류가 발생했습니다.' });
         }
     } else {
-        res.status(405).json({ message: 'Method not allowed' });
+        return res.status(405).json({ message: '허용되지 않는 메소드입니다.' });
     }
 };
