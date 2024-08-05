3b661008a298976fba74cd3b3290547f3fc1eec4
10wook <younguk137@naver.com>
2024-08-05 16:33:36 +0900

    hyw/feat/logout&blockloginpage

---
+++ src/pages/admin/logout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logout from '../components/admin/Logout';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <Logout />
+        </div>
+    );
+};
+
+export default LoginPage;
