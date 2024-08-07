7b395396b1fb6c8c2a5f8b5c9baa30a0d888ae19
SugarRaccoon <162272955+SugarRaccoon@users.noreply.github.com>
2024-08-07 16:17:04 +0900

    PaymentHistoryPageCssDesign (#132)
    fix #130
    design: 어드민 내역관리 페이지 css 디자인 완료했습니다

--- src/pages/components/admin/PaymentHistoryPage.tsx
+++ src/pages/components/admin/PaymentHistoryPage.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
+import useAuth from '@/lib/useAuth';
 
 const PaymentHistoryPage: React.FC = () => {
     const [payments, setPayments] = useState<any[]>([]);
     const [error, setError] = useState<string | null>(null);
     const [startDate, setStartDate] = useState<string>('');
     const [endDate, setEndDate] = useState<string>('');
+    const { user } = useAuth();
+    const router = useRouter();
 
     const fetchPayments = async (start: string, end: string) => {
         try {
@@ -33,66 +37,134 @@ const PaymentHistoryPage: React.FC = () => {
         }
     };
 
+    const handleAdminPaymenthistoryBtnClick = () => {
+        router.push('/admin/paymentHistory');
+    };
+
+    const handleAdminMenuBtnClick = () => {
+        const url = `/admin/${(user as any)?.id }/menu`;
+        router.push(url);
+        //?id=${id}
+    };
+
     return (
-        <div className='flex justify-center bg-orange-50' style={{ minHeight: '100vh' }}>
-            <div className='bg-white p-14 px-32  rounded-2xl shadow-xl my-20' style={{ width:'1280px'}}>
-                <h1 className='flex justify-center font-bold text-xl pb-12'>내역 관리</h1>
-                <div className='flex justify-between mb-8'>
-                    <div className='flex justify-center rounded-lg border outline-gray-700' style={{borderWidth:'2px'}}>
-                        <input 
-                            className='mx-3'
-                            type="date" 
-                            value={startDate}
-                            onChange={(e) => setStartDate(e.target.value)}
-                        />
-                        <span className='flex items-center px-4'>~</span>
-                        <input 
-                            className='mx-3'
-                            type="date" 
-                            value={endDate}
-                            onChange={(e) => setEndDate(e.target.value)}
-                            
-                        />
+        <div className='flex justify-center bg-gray-200' style={{ minHeight: '100vh' }}>
+            <div>
+                <div className='flex py-10'>
+                    <div className='flex items-center pr-10'>
+                        <div className='flex bg-white p-6 rounded-lg shadow-md' style={{width:'224px'}}>
+                            <div className='bg-black' style={{width:'60px', height:'60px'}}/>
+                            <button 
+                                className='pl-6'
+                                // onClick={() => handleButtonClick('현장결제 클릭됨')} 버튼 기능 제작 필요
+                            >
+                                마이페이지
+                            </button>
+                        </div>
                     </div>
-                    <div className='flex justify-center rounded-lg border outline-gray-700' style={{borderWidth:'2px'}}>
-                    <input 
-                        type="text" 
-                        placeholder="메뉴 검색" 
-                        style={{ padding: '10px', flex: '1' }} 
-                    />
-                    <button 
-                        className='px-4'
-                        onClick={handleSearch}
-                    >
-                        검색
-                    </button>
+                    <div className='flex items-center pr-10'>
+                        <div className='flex bg-white p-6 rounded-lg shadow-md' style={{width:'224px'}}>
+                            <div className='bg-black' style={{width:'60px', height:'60px'}}/>
+                            <button 
+                                className='pl-6'
+                                //onClick={() => handleAdminMenuBtnClick()}
+                                //마이페이지가 구현되면 라우팅 연결이 필요합니다.
+                            >
+                                가게설정
+                            </button>
+                        </div>
+                    </div>
+                    <div className='flex items-center pr-10'>
+                        <div className='flex bg-white p-6 rounded-lg shadow-md' style={{width:'224px'}}>
+                            <div className='bg-black' style={{width:'60px', height:'60px'}}/>
+                            <button 
+                                className='pl-6'
+                                //onClick={() => handleAdminPaymenthistoryBtnClick()}
+                                //상품 결제 페이지가 구현되면 라우팅 연결이 필요합니다.
+                            >
+                                상품결제
+                            </button>
+                        </div>
+                    </div>
+                    <div className='flex items-center pr-10'>
+                        <div className='flex bg-white p-6 rounded-lg shadow-md' style={{width:'224px'}}>
+                            <div className='bg-black' style={{width:'60px', height:'60px'}}/>
+                            <button 
+                                className='pl-6'
+                                onClick={() => handleAdminMenuBtnClick()}
+                            >
+                                메뉴관리
+                            </button>
+                        </div>
+                    </div>
+                    <div className='flex items-center pr-10'>
+                        <div className='flex bg-white p-6 rounded-lg shadow-md' style={{width:'224px'}}>
+                            <div className='bg-black' style={{width:'60px', height:'60px'}}/>
+                            <button 
+                                className='pl-6'
+                                onClick={() => handleAdminPaymenthistoryBtnClick()}
+                            >
+                                내역관리
+                            </button>
+                        </div>
                     </div>
                 </div>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <table className='rounded-lg mb-12 w-full border-collapse' style={{borderWidth:'2px'}}>
-                    <thead>
-                        <tr>
-                            <th className='border outline-gray-700' style={{  padding: '8px' }}>날짜</th>
-                            <th className='border outline-gray-700' style={{  padding: '8px' }}>시간</th>
-                            <th className='border outline-gray-700' style={{  padding: '8px', width:'420px' }}>결제 내역</th>
-                            <th className='border outline-gray-700' style={{  padding: '8px' }}>금액</th>
-                            <th className='border outline-gray-700' style={{  padding: '8px' }}>상세보기</th>
-                        </tr>
-                    </thead>
-                    <tbody className=''>
-                        {payments.map((payment, index) => (
-                            <tr key={index} className=''>
-                                <td className='text-center border outline-gray-700 p-2'>{payment.date}</td>
-                                <td className='text-center border outline-gray-700 p-2'>{payment.time}</td>
-                                <td className='border outline-gray-700 p-2'>{payment.details}</td>
-                                <td className='text-center border outline-gray-700 p-2'>{payment.amount}</td>
-                                <td className='text-center border outline-gray-700 p-2'><button>상세보기</button></td>
+                <div className='bg-white p-14 px-32  rounded-lg shadow-xl mb-20' style={{ width:'1280px'}}>
+                    <h1 className='flex justify-center font-bold text-xl pb-12'>내역 관리</h1>
+                    <div className='flex justify-between mb-8'>
+                        <div className='flex justify-center rounded-lg border outline-gray-700' style={{borderWidth:'2px'}}>
+                            <input 
+                                className='mx-3'
+                                type="date" 
+                                value={startDate}
+                                onChange={(e) => setStartDate(e.target.value)}
+                            />
+                            <span className='flex items-center px-4'>~</span>
+                            <input 
+                                className='mx-3'
+                                type="date" 
+                                value={endDate}
+                                onChange={(e) => setEndDate(e.target.value)}
+                                
+                            />
+                        </div>
+                        <div className='flex justify-center rounded-lg border outline-gray-700' style={{borderWidth:'2px'}}>
+                        <input 
+                            type="text" 
+                            placeholder="메뉴 검색" 
+                            style={{ padding: '10px', flex: '1' }} 
+                        />
+                        <button 
+                            className='px-4'
+                            onClick={handleSearch}
+                        >
+                            검색
+                        </button>
+                        </div>
+                    </div>
+                    {error && <p style={{ color: 'red' }}>{error}</p>}
+                    <table className='rounded-lg mb-12 w-full border-collapse' style={{borderWidth:'2px'}}>
+                        <thead>
+                            <tr>
+                                <th className='border outline-gray-700' style={{  padding: '8px' }}>날짜</th>
+                                <th className='border outline-gray-700' style={{  padding: '8px' }}>시간</th>
+                                <th className='border outline-gray-700' style={{  padding: '8px', width:'420px' }}>결제 내역</th>
+                                <th className='border outline-gray-700' style={{  padding: '8px' }}>금액</th>
+                                <th className='border outline-gray-700' style={{  padding: '8px' }}>상세보기</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div className='flex justify-end'>
-                    <button className='rounded-lg font-bold text-white w-40 h-9 bg-orange-400' onClick={() => alert('뒤로가기')}>뒤로가기</button>
+                        </thead>
+                        <tbody>
+                            {payments.map((payment, index) => (
+                                <tr key={index}>
+                                    <td className='text-center border outline-gray-700 p-2'>{payment.date}</td>
+                                    <td className='text-center border outline-gray-700 p-2'>{payment.time}</td>
+                                    <td className='border outline-gray-700 p-2'>{payment.details}</td>
+                                    <td className='text-center border outline-gray-700 p-2'>{payment.amount}</td>
+                                    <td className='text-center border outline-gray-700 p-2'><button>상세보기</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
