f43db0d6d5eba9de6feb3b51d38147ee4ac83503
LeeDongUk <144418233+leedonguk0809@users.noreply.github.com>
2024-08-07 17:51:14 +0900

    fix/userStore (#133)

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,39 +1,45 @@
-// src/pages/index.tsx
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import StoreSelect from '../components/user/StoreSelect';
 import UserHeader from '../components/UserHeader';
 import UserFooter from '../components/UserFooter';
 
-const UserMain = () => {
-    const [stores, setStores] = useState([]);
-    const [workplaceNumber, setWorkplaceNumber] = useState(1); // 기본적으로 첫 번째 사업장을 선택
+interface Store {
+  store_idx: number;
+  store_name: string;
+  store_category: string;
+  store_img_path: string;
+}
 
-    // 사업장 가게 데이터를 가져오는 함수
-    useEffect(() => {
-        const fetchStoreData = async () => {
-            try {
-                const response = await axios.get('/api/user_main_api/StoreInfo', {
-                    params: { workplaceNumber }
-                });
-                setStores(response.data);
-            } catch (error) {
-                console.error("가게 데이터를 가져오는 중 오류가 발생했습니다.", error);
-            }
-        };
+const UserMain: React.FC = () => {
+  const [stores, setStores] = useState<Store[]>([]);
+  const workplaceNumber = '1'; // 동적으로 변경할 수 있는 부분
 
-        fetchStoreData();
-    }, [workplaceNumber]);
+  useEffect(() => {
+    const fetchStoreData = async () => {
+      try {
+        const response = await axios.get<Store[]>('/api/user_main_api/StoreInfo', {
+          params: { workplaceNumber },
+        });
+        console.log('가져온 가게 데이터:', response.data); // 콘솔 로그 추가
+        setStores(response.data);
+      } catch (error) {
+        console.error('가게 데이터를 가져오는 중 오류가 발생했습니다.', error);
+      }
+    };
 
-    return (
-        <div className='h-auto bg-orange-400'>
-            <div className='flex justify-center'>
-                <UserHeader />
-            </div>
-            <StoreSelect stores={stores} />
-            <UserFooter />
-        </div>
-    );
+    fetchStoreData();
+  }, [workplaceNumber]);
+
+  return (
+    <div className='h-auto bg-orange-400'>
+      <div className='flex justify-center'>
+        <UserHeader />
+      </div>
+      <StoreSelect stores={stores} />
+      <UserFooter />
+    </div>
+  );
 };
 
 export default UserMain;
