f82295af3867c01dd3c7f9f81df0b58f66bcd1fc
SeongHyeon <tjd789123@naver.com>
2024-08-09 17:51:50 +0900

    order details not finished

---
+++ src/pages/admin/OrderDetails.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import OrderDetails from '../components/admin/OrderDetails';
+
+const PaymentHistory: React.FC = () => {
+    return <OrderDetails/>
+};
+
+export default PaymentHistory;
