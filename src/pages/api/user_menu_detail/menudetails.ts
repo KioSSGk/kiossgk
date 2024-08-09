19edaaa54040a3ab55b4fa7fe70a2bb95cbb7575
한영욱 <70461240+10wook@users.noreply.github.com>
2024-08-09 15:04:12 +0900

    hyw/feat/user/OneMenuRead (#140)
    * hyw/feat/user/OneMenuRead
    
    * hyw/del/publicFiles

--- src/pages/api/user_menu_detail/menudetails.ts
+++ src/pages/api/user_menu_detail/menudetails.ts
@@ -1,24 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import pool from '@/lib/db';
+import { RowDataPacket } from 'mysql2';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === 'GET') {
-        // 가게 정보 배열을 생성
-        const detail = [
-            {
-                id: 1,
-                name: '짜장면',
-                price: '8000',
-                description: '맛있는 짜장면',
-                category: '중식',
-                status: '주문가능',
-                image: new File([""], "짜장면.jpg", { type: 'image/jpeg' }),
-               
-            }
-    
-        ];
-
-        // 가게 정보 배열을 응답으로 반환
-        res.status(200).json(detail);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'GET') {  
+        try {
+        const { menuId } = req.query;
+        //console.log("get요청 메뉴 하나 시작");
+        //console.log(menuId);
+        const [rows] = await pool.query<RowDataPacket[]>(
+          `SELECT m.menu_idx, m.store_idx, m.menu_name, m.menu_price, m.menu_detail, m.menu_category, m.menu_status, mi.menu_image_path 
+           FROM Menu m 
+           LEFT JOIN Menuimg mi ON m.menu_idx = mi.menu_idx 
+           WHERE m.menu_Idx = ?`, [menuId]);
+          // console.log(rows);
+        res.status(200).json(rows);
+      } catch (error) {
+        console.error('메뉴 조회 중 오류 발생:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+      }
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
