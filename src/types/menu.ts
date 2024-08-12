1ca9582a77966ddc32b0cd5919ca3619bd3ef312
LeeDongUk <144418233+leedonguk0809@users.noreply.github.com>
2024-08-12 10:26:04 +0900

    lee/fix어드민 메뉴생성 (#143)
    * 충돌 해결
    
    * lee/fix/메뉴생성
    
    * fix/ 오류수정

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -9,11 +9,7 @@ export interface Menu {
   }
   
 
-  export interface Menuimg {
-    menu_img_idx: number;
-    menu_image_path: string;
-    menu_idx: number;
-  }
+
   
   export interface MenuItem extends Menu {
     image: string; // 이미지 경로 추가
