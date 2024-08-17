3685ed96e7bc4de988786bf8aa1aa0726ff4ec49
한영욱 <70461240+10wook@users.noreply.github.com>
2024-08-17 12:46:46 +0900

    Hyw/feat/user category menu (#167)
    * hyw/feat/userCategoryMenu
    
    * hyw/userCategoryMenuFix
    
    * hyw/userCategoryMenuFix

--- src/pages/components/user/StoreDetailIdx.tsx
+++ src/pages/components/user/StoreDetailIdx.tsx
@@ -13,7 +13,12 @@ interface MenuDetail {
     menu_image_path: string;
 }
 
-const StoreDetail_idx = () => {
+interface StoreDetailProps {
+    selectedCategory: string;
+}
+
+
+const StoreDetail_idx: React.FC<StoreDetailProps> = ({ selectedCategory }) =>  {
     const router = useRouter();
     const { storeId } = router.query; // URL에서 storeId를 가져옴
     const [menuItems, setMenuItems] = useState<MenuDetail[]>([]);
@@ -21,8 +26,9 @@ const StoreDetail_idx = () => {
     useEffect(() => {
         const fetchMenuDetailData = async () => {
             try {
-                const response = await axios.get(`/api/user_store_detail_api/storedetails?storeId=${storeId}`);
+                const response = await axios.get(`/api/user_store_detail_api/storedetails?storeId=${storeId}`,{ params: {selectedCategory}});
                 setMenuItems(response.data);
+                console.log(menuItems);
             } catch (error) {
                 console.error("Error fetching the store data:", error);
             }
@@ -31,7 +37,7 @@ const StoreDetail_idx = () => {
         if (storeId) {
             fetchMenuDetailData();
         }
-    }, [storeId]);
+    }, [storeId,selectedCategory]);
 
     // 메뉴 클릭 시 메뉴 상세 페이지로 이동하는 함수
     const handleMenuClick = (menuId: number) => {
@@ -42,13 +48,14 @@ const StoreDetail_idx = () => {
         <div className="h-auto bg-orange-400">
             <div className='flex justify-center min-h-screen pt-24'>
                 <div className='max-w-sm w-full'>
-                    <div className='flex overflow-x-auto whitespace-nowrap'>
+                    {/* <div className='flex overflow-x-auto whitespace-nowrap'>
                         {menuItems.map((data) => (
                             <button className='px-3 mx-2 my-1 border-2 border-white rounded-lg text-white font-bold drop-shadow-lg' key={data.menu_idx}>
                                 {data.menu_category}
                             </button>
                         ))}
-                    </div>
+                    </div> */}
+                    {/* 자체적으로 만들어져있던 카테고리는 삭제했습니다. */}
                     {menuItems.map((data) => (
                         <div className='flex items-center' key={data.menu_idx} onClick={() => handleMenuClick(data.menu_idx)} style={{ cursor: 'pointer' }}>
                             <div className='flex items-center m-2'>
