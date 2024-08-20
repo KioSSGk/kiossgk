67553e9984f9d55caaf9a37fc51a86d224f84b59
SeongHyeon <tjd789123@naver.com>
2024-08-20 13:04:27 +0900

    LSH/OrderDetails

---
+++ src/pages/admin/ViewOrderDetails.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ViewOrderDetailsPage from '../components/admin/ViewOrderDetailsPage';
+
+
+const ViewOrderDetails: React.FC = () => {
+    return <ViewOrderDetailsPage/>
+};
+
+export default ViewOrderDetails;
