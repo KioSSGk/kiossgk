1f9717df8de699fb8c27dfe926646a9267d96647
leedonguk0809 <ehddnr0481@naver.com>
2024-08-21 14:58:44 +0900

    가게 이미지 개발

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  images: {
+    domains: ['universalkiossgk.s3.ap-northeast-1.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
