dd6b9e0ea049d05f5de61ea90bd98779f5933760
한영욱 <70461240+10wook@users.noreply.github.com>
2024-08-23 10:58:48 +0900

    Hyw/feat/main page route (#213)
    * hyw/feat/mainRoute
    
    * design/change

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,15 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const JWT_SECRET = process.env.JWT_SECRET|| 'default-secret-key'; // 일단 환경 변수로 설정하고 환경 변수가 없는 경우를 대비해 입시 값을 넣어두었습니다.
 
+interface User extends JwtPayload{
+    id:number;
+    email:string;
+    iat:number;
+    exp:number;
+
+}
 // JWT 토큰 생성
 export const generateToken = (payload: object): string => {
     return jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
---
+++ src/pages/admin/[adminId]/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AdminMainPage from '../../components/admin/MainPage';
+import HeaderIcon from '../../components/admin/HeaderIcon';
+
+const AdminMain: React.FC = () => {
+    return(
+        <div className='min-h-dvh h-full bg-gray-200 '>
+            <HeaderIcon/>
+            <AdminMainPage />;
+        </div>
+    )
+};
+
+export default AdminMain;
