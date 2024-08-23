c0283c1dd88cfba2a14cd8722bf9ffeed2e0dd1a
SugarRaccoon <162272955+SugarRaccoon@users.noreply.github.com>
2024-08-23 13:49:25 +0900

    Lsh/view order complete page conect (#216)
    * rebase test
    
    * Admin Header
    
    fix #215
    
    어드민 헤더기능 구현 완료하였습니다

--- src/pages/admin/ViewOrderDetails.tsx
+++ src/pages/admin/ViewOrderDetails.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ViewOrderDetailsPage from '../components/admin/ViewOrderDetailsPage';
 import HeaderIcon from '../components/admin/HeaderIcon';
+import AdminHeader from '../components/admin/AdminHeader';
 
 
 const ViewOrderDetails: React.FC = () => {
     return (
         <div className='h-full min-h-dvh bg-gray-200 '>
+            <AdminHeader/>
             <HeaderIcon/>
             <ViewOrderDetailsPage/>
         </div>
