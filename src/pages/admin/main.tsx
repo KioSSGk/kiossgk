db42d685644e278110f371828c7e7ab0f6d96bee
한영욱 <70461240+10wook@users.noreply.github.com>
2024-08-23 10:58:48 +0900

    Hyw/feat/main page route (#213)
    * hyw/feat/mainRoute
    
    * design/change

--- src/pages/admin/main.tsx
+++ src/pages/admin/main.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminMainPage from '../components/admin/MainPage';
 import HeaderIcon from '../components/admin/HeaderIcon';
+import { useRouter } from 'next/router';
+import useAuth from '@/lib/useAuth'; // Assuming you have an Auth context set up
+interface User{
+    id:number;
+    email:string;
+    iat:number;
+    exp:number;
 
+}
 const AdminMain: React.FC = () => {
-    return(
-        <div className='min-h-dvh h-full bg-gray-200 '>
-            <HeaderIcon/>
-            <AdminMainPage />;
+   
+    const { user } = useAuth(); // Get the user from the authentication context
+    const router = useRouter();
+    const [loading, setLoading] = useState(true); // State to handle loading spinner
+
+
+    useEffect(() => {
+        if (user === null) {
+            // If user is not logged in, redirect to the login page
+            setTimeout(() => {
+            router.push('/admin/login');},2000);
+        } else {
+            // Once user is authenticated, stop the loading spinner
+            setTimeout(() => {
+            setLoading(false);
+            console.log("메인에서 불러온",user);
+            if (user !== undefined){
+            router.push(`/admin/${user.id}/main`);}},2000);
+        }
+    }, [user, router]);
+
+    if (loading) {
+        // Display a loading spinner while checking authentication status
+        return (
+            <div className="flex items-center justify-center min-h-screen bg-gray-200">
+            <div className="w-48 h-48 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin"></div>
         </div>
-    )
+        );
+    }
+
+    return (
+        <div className='min-h-dvh h-full bg-gray-200 '>
+            로그인을 다시 시도하세요.
+        </div>
+    );
 };
 
 export default AdminMain;
