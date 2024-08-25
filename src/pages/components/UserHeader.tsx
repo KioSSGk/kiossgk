f144af47ade25cab7794727a00f4ce92b629a5a4
10wook <younguk137@naver.com>
2024-08-26 00:44:33 +0900

    hyw/fix/header

--- src/pages/components/UserHeader.tsx
+++ src/pages/components/UserHeader.tsx
@@ -8,11 +8,11 @@ interface Category {
 }
 
 interface UserHeaderProps {
-  onCategoryChange ?: (category: string) => void;
-  storeId?: number; // 선택적 storeId prop
+    onCategoryChange?: (category: string) => void;
+    storeId?: number; // 선택적 storeId prop
 }
 
-export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderProps,) {
+export default function UserHeader({ storeId, onCategoryChange }: UserHeaderProps) {
     const router = useRouter();
     const [categories, setCategories] = useState<Category[]>([]);
 
@@ -30,7 +30,6 @@ export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderPro
                 apiUrl = ''; // '/user/cart'나 '/user/menu' 경로에서는 API 호출을 하지 않음
             }
 
-            console.log("가게 아이디", storeId);
             if (apiUrl === '') {
                 // 카테고리를 비우고 뒤로가기 버튼을 표시하기 위해 setCategories를 빈 배열로 설정
                 setCategories([]);
@@ -38,7 +37,6 @@ export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderPro
                 const response = await axios.get(apiUrl, {
                     params: storeId ? { storeId } : {} // storeId가 있을 경우에만 포함
                 });
-                console.log("헤더컨포넌트:",response);
                 setCategories(response.data);
             }
         } catch (error) {
@@ -50,11 +48,27 @@ export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderPro
         fetchHeaderData();
     }, [router.pathname, storeId]); // URL 경로 및 storeId가 변경될 때마다 실행
 
+    const isUserIndexPage = router.pathname.endsWith('/user'); // 유저 인덱스 페이지 여부 확인
+
     return (
         <div className='flex justify-center w-full fixed bg-teal-300 top-0 z-50 drop-shadow'>
             <div className='items-center max-w-sm w-full mx-4 pt-2'>
                 <div className='flex justify-between px-2'>
-                    <button className='h-8 w-8 bg-gray-400 items-center' onClick={() => router.push('/user')} >
+                    <button
+                        className='h-8 w-8 items-center'
+                        onClick={() => router.back()}
+                    >
+                        {/* 왼쪽 화살표 아이콘 (SVG) */}
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            className={`w-6 h-6 ${isUserIndexPage ? 'text-teal-300' : 'text-white'}`} // 유저 인덱스 페이지에서는 teal-300 색상, 그렇지 않으면 white 색상
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+                        </svg>
                     </button>
                     <div className='flex justify-end items-center h-8 w-48'>
                         <button className='drop-shadow-md text-white font-bold m-2' >검색</button>
@@ -66,18 +80,11 @@ export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderPro
                 </div>
                 <div className='flex justify-start max-w-sm w-full'>
                     <div className='flex overflow-x-auto overflow-hidden whitespace-nowrap'>
-                        {router.pathname.includes('/user/cart') || router.pathname.includes('/user/menu') ? (
-                            <button 
-                                className='px-1 m-2 text-white font-bold drop-shadow-md' 
-                                onClick={() => router.back()}
-                            >
-                                뒤로가기
-                            </button>
-                        ) : categories.length > 0 ? (
+                        {categories.length > 0 ? (
                             categories.map((category) => (
                                 <div className='' key={category.index}>
-                                    <button 
-                                        className='px-1 m-2 text-white font-bold drop-shadow-md' 
+                                    <button
+                                        className='px-1 m-2 text-white font-bold drop-shadow-md'
                                         onClick={onCategoryChange ? () => onCategoryChange(category.item) : undefined}
                                     >
                                         {category.item}
@@ -85,7 +92,7 @@ export default function UserHeader({ storeId, onCategoryChange  }: UserHeaderPro
                                 </div>
                             ))
                         ) : (
-                            <p>가게 카테고리 불러오는 중...</p>
+                            <div style={{ height: '32px' }}></div> // 공백으로 표시
                         )}
                     </div>
                 </div>
