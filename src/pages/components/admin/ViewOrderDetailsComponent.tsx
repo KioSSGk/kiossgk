e910e7cb6d235ef8d22f5ef216f81e313cc6659b
LeeDongUk <144418233+leedonguk0809@users.noreply.github.com>
2024-08-29 09:28:24 +0900

    메인 화면에 있는 주문 확인 수정 했습니다 (#253)

--- src/pages/components/admin/ViewOrderDetailsComponent.tsx
+++ src/pages/components/admin/ViewOrderDetailsComponent.tsx
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import useAuth from '@/lib/useAuth';
 
 const ViewOrderDetailsComponent: React.FC = () => {
     const [orderData, setOrderData] = useState<any[]>([]);
     const [error, setError] = useState<string | null>(null);
     const router = useRouter();
-    const [storeId, setStoreId] = useState<string | null>(null); 
     const { adminId } = router.query;
+    const [isOrderAccepted, setIsOrderAccepted] = useState<{ [key: number]: boolean }>({});
 
     const fetchOrders = async () => {
         try {
             const response = await axios.get('/api/admin_view_order_details_api/order', {
-                params: { storeId: storeId }
+                params: { storeId: adminId }
             });
-            console.log('API 응답:', response.data);
-            setOrderData(response.data);
+            const data = response.data;
+            setOrderData(data);
+
+            // 주문 상태를 초기화
+            const acceptedOrders = data.reduce((acc: { [key: number]: boolean }, order: any) => {
+                acc[order.order_idx] = order.order_state === '03'; // 상태가 '03'이면 접수된 상태로 설정
+                return acc;
+            }, {});
+            setIsOrderAccepted(acceptedOrders);
+
             setError(null);
-            console.log(orderData)
         } catch (error) {
             console.error('Error fetching view orders:', error);
             setError('주문 목록을 불러오는데 실패했습니다.');
@@ -27,17 +33,47 @@ const ViewOrderDetailsComponent: React.FC = () => {
 
     useEffect(() => {
         if (adminId) {
-            setStoreId(adminId as string);  // adminId를 storeId로 설정
+            fetchOrders();
         }
     }, [adminId]);
 
-    useEffect(() => {
-        if (storeId) {
+    const handleOrderAccept = async (orderId: number) => {
+        try {
+            await axios.put('/api/admin_view_order_details_api/order', {
+                orderId: orderId,
+                orderState: '03'
+            });
+            setIsOrderAccepted(prevState => ({
+                ...prevState,
+                [orderId]: true,  // 주문 접수 시 상태 변경
+            }));
+            await axios.post('/api/notice/send_order_notification', {
+                orderId: orderId,
+                action: 'accept'
+            });
             fetchOrders();
+        } catch (error) {
+            console.error('주문 접수 업데이트 에러가 발생했습니다', error);
         }
-    }, [storeId]);
+    };
 
     const handleCookingComplete = async (orderId: number) => {
+        try {
+            await axios.put('/api/admin_view_order_details_api/order', {
+                orderId: orderId,
+                orderState: '04'
+            });
+            await axios.post('/api/notice/send_order_notification', {
+                orderId: orderId,
+                action: 'complete'
+            });
+            fetchOrders();
+        } catch (error) {
+            console.error('조리완료 업데이트 에러가 발생했습니다', error);
+        }
+    };
+
+    const handleOrderCancel = async (orderId: number) => {
         try {
             await axios.put('/api/admin_view_order_details_api/order', {
                 orderId: orderId,
@@ -45,21 +81,9 @@ const ViewOrderDetailsComponent: React.FC = () => {
             });
             fetchOrders();
         } catch (error) {
-            console.error('조리완료 업데이트 에러가 발생했습니다', error);
+            console.error('주문 취소 중 오류가 발생했습니다', error);
         }
-    }
-
-        const handleCookingCancel = async (orderId: number) => {
-        try {
-            await axios.put('/api/admin_view_order_details_api/order', {
-                orderId: orderId,
-                orderState: '04'
-            });
-            fetchOrders();
-        } catch (error) {
-            console.error('조리완료 업데이트 에러가 발생했습니다', error);
-        }
-    }
+    };
 
     return (
         <div className='flex justify-center'>
@@ -67,53 +91,63 @@ const ViewOrderDetailsComponent: React.FC = () => {
                 <div>
                     {error && <p style={{ color: 'red' }}>{error}</p>}
                     <div>
-                            {orderData.map((order, index) => (
-                                <div key={index} className=''>
-                                    <div className='justify-between bg-white px-4 mb-2 min-w-[488px]'>
-                                        <div className='border-[1px] border-gray-200 rounded-md px-6 py-3'>
-                                            <div className='flex justify-between py-2'>
-                                                <div>
-                                                    주문번호 : 
-                                                </div>
-                                                <div>
-                                                    {order.order_idx}
-                                                </div>
-                                            </div>
-                                            <div className='flex justify-between py-2'>
-                                                <div>
-                                                    주문시간 :
-                                                </div>
-                                                <div>
-                                                    {order.created}
-                                                </div>
-                                            </div>
-                                            <div className='font-bold flex justify-between pt-2 pb-8'>
-                                                <div>
-                                                    {order.menu_name}&nbsp;
-                                                </div>
-                                                <div>
-                                                    x {order.count}
-                                                </div>
-                                            </div>
-                                            <div className='flex justify-between py-2'>
-                                                <button
-                                                    className='bg-indigo-500 text-white font-bold rounded-md w-[180px] h-[36px]'
-                                                    
-                                                    onClick={()=>handleCookingComplete(order.order_idx)}
-                                                    >
-                                                    조리완료
-                                                </button>
-                                                <button 
-                                                    className='bg-indigo-500 text-white font-bold rounded-md w-[180px] h-[36px]'
-                                                    onClick={()=>handleCookingCancel(order.order_idx)}
-                                                    >
-                                                    주문취소
-                                                </button>
-                                            </div>
+                        {orderData.map((order, index) => (
+                            <div key={index} className='justify-between bg-white px-4 mb-2 min-w-[488px]'>
+                                <div className='border-[1px] border-gray-200 rounded-md px-6 py-3'>
+                                    <div className='flex justify-between py-2'>
+                                        <div>
+                                            주문번호 : 
+                                        </div>
+                                        <div>
+                                            {order.order_idx}
                                         </div>
                                     </div>
+                                    <div className='flex justify-between py-2'>
+                                        <div>
+                                            주문시간 :
+                                        </div>
+                                        <div>
+                                            {order.created}
+                                        </div>
+                                    </div>
+                                    <div className='font-bold flex justify-between pt-2 pb-8'>
+                                        <div>
+                                            {order.menu_name}&nbsp;
+                                        </div>
+                                        <div>
+                                            x {order.count}
+                                        </div>
+                                    </div>
+                                    <div className='flex justify-between py-2'>
+                                        {!isOrderAccepted[order.order_idx] ? (
+                                            <>
+                                                <button
+                                                    className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
+                                                    onClick={() => handleOrderAccept(order.order_idx)}
+                                                >
+                                                    주문 접수
+                                                </button>
+                                                <button
+                                                    className="bg-red-500 text-white py-2 px-4 rounded mx-2 hover:bg-red-600 transition duration-200"
+                                                    onClick={() => handleOrderCancel(order.order_idx)}
+                                                >
+                                                    주문 취소
+                                                </button>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <button
+                                                    className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600 transition duration-200"
+                                                    onClick={() => handleCookingComplete(order.order_idx)}
+                                                >
+                                                    조리완료
+                                                </button>
+                                            </>
+                                        )}
+                                    </div>
                                 </div>
-                            ))}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
